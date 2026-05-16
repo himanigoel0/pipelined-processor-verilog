5e32732a26531358215007443159d9548ac4ba68
himanigoel0 <24110134@iitgn.ac.in>
2026-05-16 16:49:04 +0530

    Add files via upload

---
+++ execute_preprocessor.v
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+
+module execute_preprocessor(
+input clk, rst, enable_ex,
+input [31:0] src1, src2, imm, mem_data_read_in,
+input [6:0] control_in,
+
+output reg [31:0] aluin1, aluin2,
+output reg [2:0] operation_out, opselect_out,
+output reg [4:0] shift_number,
+output reg enable_arith, enable_shift,
+output mem_data_wr_en, 
+output [31:0] mem_data_write_out
+    );
+    
+// All these outputs are sequential:
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        aluin1 <= 0;
+        aluin2 <= 0;
+        operation_out <= 0;
+        opselect_out <= 0;
+        shift_number <= 0;
+        enable_arith <= 0;
+        enable_shift <= 0;
+    end
+    
+    else begin
+        if (enable_ex) begin
+        
+            // aluin1:
+            aluin1 <= src1;
+            
+            // aluin2:
+            if (control_in[3] && (control_in[2:0] == 3'b001)) aluin2 <= imm;                    // Arith_logic
+            else if ((control_in[3] == 0) && (control_in[2:0] == 3'b001)) aluin2 <= src2;       // Arith_logic
+            else if (control_in[3] && (control_in[2:0] == 3'b101)) aluin2 <= mem_data_read_in;  // Mem Read
+            
+            // operation_out and opselect_out:
+            operation_out <= control_in[6:4];
+            opselect_out <= control_in[2:0];
+            
+            // shift_number, opselect = shift_reg: 
+            if (control_in[2:0] == 3'b000 && imm[2] == 0) shift_number <= imm[10:6];
+            else if (control_in[2:0] == 3'b000 && imm[2] == 1) shift_number <= src2[4:0];
+            else shift_number <= 0;
+            
+            // enable_arith: 
+            // arith logic:
+            if (control_in[2:0] == 3'b001) enable_arith <= 1'b1; // Irrespective of control_in[3]
+            // mem_read:
+            else if (control_in[2:0] == 3'b101 && control_in[3] == 0) enable_arith <= 1'b0;
+            else if (control_in[2:0] == 3'b101 && control_in[3] == 1) enable_arith <= 1'b1;
+            else enable_arith <= 0;
+            
+            // enable_shift, opselect = shift_reg:
+            if (control_in[2:0] == 3'b000) enable_shift <= 1;
+            else enable_shift <= 0;
+            
+        end
+        
+        else if (enable_ex == 0) begin
+        
+            // Shift_number:
+            shift_number <= 0;
+            
+            // Enable_arith:
+            enable_arith <= 0;
+            
+            // Enable_shift:
+            enable_shift <= 0;
+        
+        end
+    end
+end
+
+// Because they are combinational:
+assign mem_data_wr_en = ((control_in[2:0] == 3'b100) && (control_in[3] == 1));
+assign mem_data_write_out = src2;
+
+endmodule
+
